a988ca16ee2412178b3bafb237d7ec6780136019
jaygee111 <jackgrzegorzyk@gmail.com>
2024-04-25 12:15:14 -0700

    lab5 done

--- lab4/js/lab.js
+++ lab4/js/lab.js
@@ -1,6 +1,26 @@
-// index.js - purpose and description here
-// Author: Your Name
-// Date:
+/**
+ //Rock, Paper, Scissors.
+ 
+ //Pseudocode of Rock, Paper, Scissors when implemented into JavaScript.
+ 
+ // @link   https://JackGrz.github.io/art101a/lab4/index.html
+ //@author Jack Grzegorzyk.
+ //@since  04/24/2024
+ */
+
+// Have two players.
+	// Designate a Player 1 and a Player 2
+// Winner of the game is decided by winning two out of three matches.
+// Each player has three tools to use.
+	// Rock, Paper, or Scissors
+// Players simultaneously choose one of the three choices on the count of three.
+// The winner of each mach is dependent on the choice of each player
+	// Rock wins against Scissors
+	// Scissors wins against Paper
+	// Paper wins against Rock
+// A tie will happen when each player makes the same choice.
+// When there is a tie the match will continue till one player beets the other.
+					
 
 // Constants
 
--- lab5/js/lab.js
+++ lab5/js/lab.js
@@ -1,21 +1,21 @@
-// index.js - purpose and description here
-// Author: Your Name
-// Date:
+/*
+  *Lab 5: Data Types and Variables
+  *Author: Jack Grzegorzyk
+  *Date: April 25, 2024
+*/
 
-// Constants
+//  Define Variables
+var make = "Toyota";
+var model = "Corolla LE";
+var color = "pearlescent white";
+var year = 2019;
 
-// Functions
+//  Calculate
+var age = 2024 - year;
 
-// this is an example function and this comment tells what it doees and what parameters are passed to it.
-function myFunction(param1, param2) {
-  // some code here
-  // return results;
-}
-
-function main() {
-  console.log("Main function started.");
-  // the code that makes everything happen
-}
-
-// let's get this party started
-main();
+// Output
+document.writeln("Make: " + make + "<br>");
+document.writeln("Model: " + model + "<br>");
+document.writeln("Color: " + color + "<br>");
+document.writeln("Year: " + year + "<br>");
+document.writeln("Age: " + age + " years<br>");
