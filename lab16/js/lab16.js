16a55bf1afcb8cb4e17d0fb6a817762c26f5a756
jaygee111 <jackgrzegorzyk@gmail.com>
2024-06-06 22:22:42 -0700

    final lab done!

--- lab16/js/lab16.js
+++ lab16/js/lab16.js
@@ -1,22 +1,34 @@
 // Lab 16: JSON and APIs
 // Author: Jack Grzegorzyk
 // Date: June 7th, 2024
- // Description: 
+// Description: Working with your partner, experiment with processing JSON from an API.
 
- $(document).ready(function() {
-  $("#activate").click(function() {
-      $.ajax({
-          url: "https://yesno.wtf/api/",
-          type: "GET",
-          dataType: "json",
-          success: function(data) {
-              $("#output").html("<p>Answer: " + data.answer + "</p><img src='" + data.image + "' alt='Yes or No image'>");
-          },
-          error: function(jqXHR, textStatus, errorThrown) {
-              console.log("Error:", textStatus, errorThrown);
-          }
-      });
-  });
-});
+const comicData = {
+    "title": "Fluid Speech",
+    "alt": "Thank you to linguist Gretchen McCulloch for teaching me about phonetic assimilation, and for teaching me that if you stand around in public reading texts from a linguist and murmuring example phrases to yourself, people will eventually ask if you're okay.",
+    "img": "https://imgs.xkcd.com/comics/fluid_speech.png"
+};
 
-``
+function ajaxSuccess(data) {
+    console.log("Results", data);
+    $("#output").empty();
+
+    const titleElement = $("<h1>").text(comicData.title);
+    $("#output").append(titleElement);
+
+    const imageElement = $("<img>").attr({
+        "src": comicData.img,
+        "alt": comicData.alt,
+        "title": comicData.alt
+    });
+
+    $("#output").append(imageElement);
+    const captionElement = $("<p>").text(comicData.alt);
+    $("#output").append(captionElement);
+}
+
+function ajaxError(request, error) {
+    console.log("Oopsie!", request, error);
+}
+
+ajaxSuccess(comicData);
